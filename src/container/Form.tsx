462ca6f5ead34d270a7fbc02f447b9a3db8f5f60
Chen Chen <talent_prince@163.com>
2020-04-12 17:21:48 +0800

    Add related select

--- src/container/Form.tsx
+++ src/container/Form.tsx
@@ -40,14 +40,20 @@ export class Form extends React.Component<FormPros, FormState> {
         const inputFields = ["Name", "Password"].map((label) =>
             <InputField onChange={this.onChange} label={label} value={this.state[label]}/>
         );
-        const radioLable = "Gender"
-        const selectLabel = "Cert type"
-        const multiLabel = "Role"
+        const genderLable = "Gender"
+        const certLabel = "Cert type"
+        const roleLabel = "Role"
+        const payTypeLabel = "Pay Type"
+        const issuerLabel = "Issuer"
+        const payType = this.state[payTypeLabel]
+        const issuerOption = (payType === "credit card" || payType === undefined) ? ["Master", "Visa", "Union Pay"] : ["Wechat", "Alipay"]
         return <form onSubmit={this.onSubmit}>
             {inputFields}
-            <Radio label={radioLable} selected={this.state[radioLable]} options={["Male", "Female"]} onChanged={this.onChange}/>
-            <Select label={selectLabel} selected={this.state[selectLabel]} options={["SMS", "Token", "Token+SMS"]} onChanged={this.onChange}/>
-            <MultiSelect label={multiLabel} selected={this.parseMultiSelected()} options={["Read", "Write"]} onChanged={this.onChange}/>
+            <Radio label={genderLable} selected={this.state[genderLable]} options={["Male", "Female"]} onChanged={this.onChange}/>
+            <Select label={certLabel} selected={this.state[certLabel]} options={["SMS", "Token", "Token+SMS"]} onChanged={this.onChange}/>
+            <MultiSelect label={roleLabel} selected={this.parseMultiSelected()} options={["Read", "Write"]} onChanged={this.onChange}/>
+            <Select label={payTypeLabel} selected={this.state[payTypeLabel]} options={["Credit Card", "E-Pay"]} onChanged={this.onChange}/>
+            <Select label={issuerLabel} selected={this.state[issuerLabel]} options={issuerOption} onChanged={this.onChange}/>
             <Button text="Submit"/>
         </form>
     }
